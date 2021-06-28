97e6a1c3f8e8c5531a07794b8a6325a7e63616ce
NazarK0 <staticman999@gmail.com>
2021-06-28 14:55:53 +0300

    production

---
+++ adminPanel/light/assets/js/myScripts/toggle-on-hover.js
@@ -0,0 +1,12 @@
+// var hoverItemsList = document.querySelector(".hover-item");
+
+// hoverItemsList.forEach(item => {
+//     item.addEventListener("mouseover", function({ target }) {
+//         var toggleBlock = hoverItem.querySelector(".toggle-block");
+
+//         toggleBlock.style.display = "flex";
+//         hoverItem.addEventListener("mouseout", function() {
+//             toggleBlock.style.display = "none";
+//         });
+//     });
+// })
