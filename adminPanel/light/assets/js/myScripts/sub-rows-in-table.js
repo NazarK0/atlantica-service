a322d1c74cade828940d1561c9f11bba5590940c
Nazar Vanivskyi <staticman999@gmail.com>
2021-07-01 12:24:28 +0300

    ref-books

--- adminPanel/light/assets/js/myScripts/sub-rows-in-table.js
+++ adminPanel/light/assets/js/myScripts/sub-rows-in-table.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
     var table = $('#example').DataTable({
-        "ajax": "../../light/assets/data/objects.json",
+        "ajax": "../../light/assets/data/reference-books.json",
         "columns": [{
                 "className": 'details-control',
                 "orderable": false,
                 "data": null,
                 "defaultContent": ''
             },
-            { "data": "name" },
+            { "data": "title" },
         ],
         "order": [
             [1, 'asc']
@@ -25,8 +25,33 @@ $(document).ready(function() {
             tr.removeClass('shown');
         } else {
             // Open this row
-            row.child(format(row.data())).show();
+            row.child(rbFormat(row.data())).show();
             tr.addClass('shown');
         }
     });
 });
+
+function rbFormat(d) {
+    // `d` is the original data object for the row
+    var rows = function(subRefBooks) {
+        return subRefBooks.map(function(book){
+            return (
+                '<tr>' + 
+                    '<td>' + book.title + '</td>' +
+                '</tr>'
+            )
+        }).join('');
+    }
+    console.log(d, 'RBD')
+    return (
+    '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;" class="subtable">' +
+        '<thead>' +
+            '<tr>' +
+                '<th>Наименование</th>' +
+            '</tr>' + 
+        '</thead>' +
+        '<tbody>' +
+          rows(d.subRefBooks) +
+        '</tbody>' +
+    '</table>');
+}
