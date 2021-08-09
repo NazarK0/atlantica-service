395bb50c40bc64021ffe9e2d93e88f33dec860c7
Nazar Vanivskyi <staticman999@gmail.com>
2021-08-09 09:08:18 +0300

    contact page

--- landing2/src/js/animations.js
+++ landing2/src/js/animations.js
@@ -3,10 +3,11 @@ const menuItems = document.querySelectorAll('.block.menu .menu>.menu-item');
 menuItems.forEach(item => {
   item.addEventListener('mouseover', () => {
     const submenu = item.querySelector('.dropdown-content')
-    submenu.classList.add('animate__fadeInUp');
+    submenu?.classList.add('animate__fadeInUp');
+    
   });
   item.addEventListener('mouseleave', () => {
     const submenu = item.querySelector('.dropdown-content')
-    submenu.classList.remove('animate__fadeInUp');
+    submenu?.classList.remove('animate__fadeInUp');
   });
 })
