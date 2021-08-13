f90ca501542adcefd2f467bd1ab690351e9004e9
Nazar Vanivskyi <staticman999@gmail.com>
2021-08-13 13:11:12 +0300

    index

--- landing2/src/js/animationsIndex.js
+++ landing2/src/js/animationsIndex.js
@@ -3,7 +3,7 @@ const statisticBranches = document.getElementById('stat-branches');
 const statisticClients = document.getElementById('stat-clients');
 const statisticEmploees = document.getElementById('stat-emploees');
 
-// animateNumber(statisticProjects, 0, statisticProjects.innerText, 2000);
-// animateNumber(statisticBranches, 0, statisticBranches.innerText, 2000);
-// animateNumber(statisticClients, 0, statisticClients.innerText, 2000);
-// animateNumber(statisticEmploees, 0, statisticEmploees.innerText, 2000);
+animateNumber(statisticProjects, 0, statisticProjects.innerText, 2000);
+animateNumber(statisticBranches, 0, statisticBranches.innerText, 2000);
+animateNumber(statisticClients, 0, statisticClients.innerText, 2000);
+animateNumber(statisticEmploees, 0, statisticEmploees.innerText, 2000);
