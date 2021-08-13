22c7b76c0daa83751b9d6e67cbcbd0104bbbaa45
Nazar Vanivskyi <staticman999@gmail.com>
2021-08-13 12:18:22 +0300

    index

---
+++ landing2/src/js/animationsAboutUs.js
@@ -0,0 +1,9 @@
+const statisticProjects = document.getElementById('stat-projects');
+const statisticBranches = document.getElementById('stat-branches');
+const statisticClients = document.getElementById('stat-clients');
+const statisticEmploees = document.getElementById('stat-emploees');
+
+animateNumber(statisticProjects, 0, statisticProjects.innerText, 2000);
+animateNumber(statisticBranches, 0, statisticBranches.innerText, 2000);
+animateNumber(statisticClients, 0, statisticClients.innerText, 2000);
+animateNumber(statisticEmploees, 0, statisticEmploees.innerText, 2000);
