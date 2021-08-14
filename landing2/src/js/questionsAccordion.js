805977f1235ca3c1a37bbc37564b652369bbee90
Nazar Vanivskyi <staticman999@gmail.com>
2021-08-14 08:50:11 +0300

    index page

--- landing2/src/js/questionsAccordion.js
+++ landing2/src/js/questionsAccordion.js
@@ -3,12 +3,31 @@ let i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
+    closePanels(this);
     this.classList.toggle("active");
-    var panel = this.nextElementSibling;
+    const panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
+      panel.style.display = "none";
+      console.log('active has maxHeight')
     } else {
+      panel.style.display = "block";
       panel.style.maxHeight = panel.scrollHeight + "px";
+      console.log('active hasn`t maxHeight')
     }
   });
+}
+
+function closePanels(exceptThis) {
+  console.log('close Panels')
+  for (i = 0; i < acc.length; i++) {
+    if (acc[i] !== exceptThis) {
+      acc[i].classList.remove("active");
+      const panel = acc[i].nextElementSibling;
+
+      panel.style.maxHeight = null;
+      panel.style.display = "none";
+    }
+    
+  }
 }
